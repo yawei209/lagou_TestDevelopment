7e7ebe623a9da4c10c668150000195b220f50f9d
zhangyawei <yawei_zhang@bizconf.cn>
2021-02-12 12:34:29 +0800

    学习了unitest测试框架：suite.addTest(类名("用例名"))

---
+++ test_unittest.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/12 0:53
+# @Author : zhangyawei
+# @Email : zhangyawei209@163.com
+# @File : test_unittest.py
+# @Software: PyCharm
+
+
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+
+    #setUp和 tearDown方法是在每条测试用例前后分别调用的
+    def setUp(self):
+        print("setup")
+
+    def tearDown(self):
+        print("teardown\n")
+    @classmethod
+    def setUpClass(cls) -> None:
+        print("set up class")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print("tear down class")
+
+    def test_abc(self):
+        print("test abc")
+
+    def test_upper(self):
+        print("test_upper")
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        print("test_isupper")
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        print('test_split')
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    #unitest.main() 执行所有测试用例（test_开头的）
+    # unittest.main()
+
+    #创建一个测试套件，testsuite,只执行添加的一条用例、或几条。
+    suite = unittest.TestSuite()
+    suite.addTest(TestDemo("test_abc"))      #addTest(类名("用例名"))
+    unittest.TextTestRunner().run(suite)    #然后运行测试套件
