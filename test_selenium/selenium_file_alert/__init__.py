cd60b2f946d72b4c1014c616104445be24cbfefe
yawei209 <zhangyawei209@163.com>
2021-02-22 23:17:56 +0800

    元素为input的方法send_keys()可以上传文件

---
+++ test_selenium/selenium_file_alert/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/10 11:30
+# @Author : zhangyawei
+# @Email : zhangyawei209@163.com
+# @File : second.py
+# @Software: PyCharm
---
+++ test_selenium/selenium_file_alert/test_file.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/10 11:30
+# @Author : zhangyawei
+# @Email : zhangyawei209@163.com
+# @File : second.py
+# @Software: PyCharm
+from time import sleep
+
+from test_selenium.selenium_file_alert.base import Base
+
+# class TestFile(Base):
+#     def test_file(self):
+#         self.driver.get("https://image.baidu.com")
+#         self.driver.find_element_by_xpath('//*[@id="sttb"]/img[1]')
+#         sleep(1)
+#         self.driver.find_element_by_id('id="uploadImg"').send_keys('BMW.jpeg') #元素为input的方法send_keys()可以上传文件
+#         sleep(1)
